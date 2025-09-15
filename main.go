4e9013400ae884cb4d9cb1ed382b8715e621ab7d
goesbams <goesbamsday@gmail.com>
2025-09-15 15:56:51 +0700

    setup basic server with API routes

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Routes Setup
+	http.HandleFunc("/config", handlers.CreateConfig)
+	http.HandleFunc("/config/update", handlers.UpdateConfig)
+	http.HandleFunc("/config/rollback", handlers.RollbackConfig)
+	http.HandleFunc("/config/fetch", handlers.FetchConfig)
+	http.HandleFunc("/config/versions", handlers.ListVersionsHandler)
+
+	fmt.Println("Server started at :8090")
+	log.Fatal(http.ListenAndServe(":8090", nil))
+}
