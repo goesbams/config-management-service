25d7445241dac948846af360b58dac67827a61cd
goesbams <goesbamsday@gmail.com>
2025-09-16 17:02:01 +0700

    unit test create config implementation

---
+++ config/tests/config_test.go
@@ -0,0 +1,166 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/goesbams/config-management-service/handlers"
+	"github.com/goesbams/config-management-service/models"
+)
+
+func TestCreateConfig(t *testing.T) {
+	t.Run("success create new config", func(t *testing.T) {
+		config := models.Config{
+			Name: "Main Database Config",
+			Type: "DATABASE",
+			Versions: []models.ConfigVersion{
+				{
+					Version: 1,
+					Property: map[string]interface{}{
+						"host":     "localhost",
+						"port":     5432,
+						"username": "admin",
+						"password": "secret",
+					},
+				},
+			},
+		}
+
+		configData, _ := json.Marshal(config)
+
+		req, err := http.NewRequest("POST", "/config", bytes.NewBuffer(configData))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(handlers.CreateConfig)
+
+		handler.ServeHTTP(rr, req)
+
+		var actualConfig models.Config
+		err = json.NewDecoder(rr.Body).Decode(&actualConfig)
+		assert.NoError(t, err)
+		assert.Equal(t, config.Name, actualConfig.Name)
+		assert.Equal(t, config.Type, actualConfig.Type)
+	})
+
+	t.Run("error when config name is empty", func(t *testing.T) {
+		config := models.Config{
+			Name: "",
+			Type: "DATABASE",
+			Versions: []models.ConfigVersion{
+				{
+					Version: 1,
+					Property: map[string]interface{}{
+						"host":     "localhost",
+						"port":     5432,
+						"username": "admin",
+						"password": "secret",
+					},
+				},
+			},
+		}
+
+		configData, _ := json.Marshal(config)
+
+		req, err := http.NewRequest("POST", "/config", bytes.NewBuffer(configData))
+		assert.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(handlers.CreateConfig)
+
+		handler.ServeHTTP(rr, req)
+
+		assert.Equal(t, http.StatusBadRequest, rr.Code)
+		assert.Equal(t, "config name is required", strings.TrimSpace(rr.Body.String()))
+	})
+
+	t.Run("error when config type is empty", func(t *testing.T) {
+		config := models.Config{
+			Name: "Main Database Config",
+		}
+
+		configData, _ := json.Marshal(config)
+
+		req, err := http.NewRequest("POST", "/config", bytes.NewBuffer(configData))
+		assert.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(handlers.CreateConfig)
+
+		handler.ServeHTTP(rr, req)
+
+		assert.Equal(t, http.StatusBadRequest, rr.Code)
+		assert.Equal(t, "config type is required", strings.TrimSpace(rr.Body.String()))
+	})
+
+	t.Run("error when config type is invalid", func(t *testing.T) {
+		config := models.Config{
+			Name: "Main Database Config",
+			Type: "OTHERS",
+		}
+
+		configData, _ := json.Marshal(config)
+
+		req, err := http.NewRequest("POST", "/config", bytes.NewBuffer(configData))
+		assert.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(handlers.CreateConfig)
+
+		handler.ServeHTTP(rr, req)
+
+		assert.Equal(t, http.StatusBadRequest, rr.Code)
+		fmt.Println(rr.Body.String())
+		assert.Equal(t, "invalid config type", strings.TrimSpace(rr.Body.String()))
+	})
+
+	t.Run("error when versions is nil", func(t *testing.T) {
+		config := models.Config{
+			Name: "Main Database Config",
+			Type: "DATABASE",
+		}
+
+		configData, _ := json.Marshal(config)
+
+		req, err := http.NewRequest("POST", "/config", bytes.NewBuffer(configData))
+		assert.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(handlers.CreateConfig)
+
+		handler.ServeHTTP(rr, req)
+
+		assert.Equal(t, http.StatusBadRequest, rr.Code)
+		assert.Contains(t, rr.Body.String(), "invalid config versions")
+	})
+
+	t.Run("Error: Config versions length is 0", func(t *testing.T) {
+		config := models.Config{
+			Name:     "Main Database Config",
+			Type:     "DATABASE",
+			Versions: []models.ConfigVersion{},
+		}
+
+		configData, _ := json.Marshal(config)
+
+		req, err := http.NewRequest("POST", "/config", bytes.NewBuffer(configData))
+		assert.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(handlers.CreateConfig)
+
+		handler.ServeHTTP(rr, req)
+
+		assert.Equal(t, http.StatusBadRequest, rr.Code)
+		assert.Contains(t, rr.Body.String(), "config versions is required")
+	})
+}
