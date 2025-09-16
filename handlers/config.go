17c47c213522a0aa964e524e4a7e49a5c8326418
goesbams <goesbamsday@gmail.com>
2025-09-16 14:52:11 +0700

    re-implement rollback config

--- handlers/config.go
+++ handlers/config.go
@@ -34,7 +34,6 @@ func CreateConfig(w http.ResponseWriter, r *http.Request) {
 	// store new config
 	configStore[newConfig.Name] = newConfig
 
-	// config created response
 	log.Printf("new config created: %s", newConfig.Name)
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newConfig)
@@ -83,55 +82,75 @@ func UpdateConfig(w http.ResponseWriter, r *http.Request) {
 }
 
 func RollbackConfig(w http.ResponseWriter, r *http.Request) {
-	// log.Println("request rollback config")
+	log.Println("request rollback config")
 
-	// var rollbackRequest struct {
-	// 	Name    string `json:"name"`
-	// 	Version int    `json:"version"`
-	// }
+	var rollbackRequest struct {
+		Name    string `json:"name"`
+		Version int    `json:"version"`
+	}
 
-	// err := json.NewDecoder(r.Body).Decode(&rollbackRequest)
-	// if err != nil {
-	// 	log.Println("error decoding request body:", err)
-	// 	http.Error(w, "invalid json", http.StatusBadRequest)
-	// 	return
-	// }
+	err := json.NewDecoder(r.Body).Decode(&rollbackRequest)
+	if err != nil {
+		log.Println("error decoding request body:", err)
+		http.Error(w, "invalid json", http.StatusBadRequest)
+		return
+	}
 
-	// existingConfig, exists := configStore[rollbackRequest.Name]
-	// if !exists || rollbackRequest.Version >= existingConfig.Version {
-	// 	log.Println("invalid version")
-	// 	http.Error(w, "invalid version", http.StatusBadRequest)
-	// 	return
-	// }
+	// retrieve existing config
+	existingConfig, found := configStore[rollbackRequest.Name]
+	if !found {
+		log.Println("config not found")
+		http.Error(w, "config not found", http.StatusBadRequest)
+		return
+	}
 
-	// rollbackConfig := existingConfig
-	// rollbackConfig.Version = rollbackRequest.Version
-	// configStore[rollbackRequest.Name] = rollbackConfig
+	// check if version exists & is less than current version
+	ev := existingConfig.Versions
+	if rollbackRequest.Version <= 0 || rollbackRequest.Version >= ev[len(ev)-1].Version {
+		log.Println("invalid version request")
+		http.Error(w, "invalid version", http.StatusBadRequest)
+		return
+	}
 
-	// log.Printf("config rollback: %s, version: %d", rollbackConfig.Name, rollbackConfig.Version)
-	// w.WriteHeader(http.StatusOK)
-	// json.NewEncoder(w).Encode(rollbackConfig)
+	// get specific version
+	rollbackVersion := ev[rollbackRequest.Version-1]
+
+	// create new version
+	newVersion := ev[len(ev)-1].Version + 1
+
+	// update rollback config
+	rollbackConfig := existingConfig
+	rollbackConfig.Versions = append(rollbackConfig.Versions,
+		models.ConfigVersion{
+			Version:  newVersion,
+			Property: rollbackVersion.Property,
+		},
+	)
+
+	log.Printf("config rollback: %s, version: %d", rollbackConfig.Name, newVersion)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(rollbackConfig)
 }
 
 func FetchConfig(w http.ResponseWriter, r *http.Request) {
-	// log.Println("request fetch config")
+	log.Println("request fetch config")
 
-	// configName := r.URL.Query().Get("name")
-	// if configName == "" {
-	// 	http.Error(w, "config name is required", http.StatusBadRequest)
-	// 	return
-	// }
+	configName := r.URL.Query().Get("name")
+	if configName == "" {
+		http.Error(w, "config name is required", http.StatusBadRequest)
+		return
+	}
 
-	// config, exists := configStore[configName]
-	// if !exists {
-	// 	log.Println(w, "config not found")
-	// 	http.Error(w, "config not found", http.StatusNotFound)
-	// 	return
-	// }
+	config, exists := configStore[configName]
+	if !exists {
+		log.Println(w, "config not found")
+		http.Error(w, "config not found", http.StatusNotFound)
+		return
+	}
 
-	// log.Printf("latest config: %s", configName)
-	// w.WriteHeader(http.StatusOK)
-	// json.NewEncoder(w).Encode(config)
+	log.Printf("latest config: %s", configName)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(config)
 }
 
 func ListVersionsHandler(w http.ResponseWriter, r *http.Request) {
